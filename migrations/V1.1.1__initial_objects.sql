afa20d3a8e1c61be64599d47f41601b9e1e305f7
Aravind Narayana LNU <alnu993@r1rcm.com>
2022-08-14 11:59:49 +0530

    First 2 schema scripts

---
+++ migrations/V1.1.1__initial_objects.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE SCHEMA "RCM"."RCM_STAGING_DV"
+COMMENT='Schema to store all the staging objects';
+
+CREATE OR REPLACE SCHEMA "RCM"."RCM_RAW_DV"
+COMMENT='Schema to store all the raw vault objects';
